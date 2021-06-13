8eef99bfc63f199e4b1cebb38d956e224a283eda
Pradeep Kumar <pradeepkumar0509@gmail.com>
2021-06-13 20:43:29 +0530

    Created project structure

--- src/main/java/in/devopsbuddy/web/controller/HelloController.java
+++ src/main/java/in/devopsbuddy/web/controller/HelloController.java
@@ -1,4 +1,4 @@
-package in.devopsbuddy.controller;
+package in.devopsbuddy.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;

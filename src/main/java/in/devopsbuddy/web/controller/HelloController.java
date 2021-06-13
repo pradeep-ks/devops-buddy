be94734b5268bdefd5a153b311318582fe1110a2
Pradeep Kumar <pradeepkumar0509@gmail.com>
2021-06-13 21:05:56 +0530

    Added placeholder index page

--- src/main/java/in/devopsbuddy/web/controller/HelloController.java
+++ src/main/java/in/devopsbuddy/web/controller/HelloController.java
@@ -1,13 +1,12 @@
 package in.devopsbuddy.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
     
-    @RequestMapping("/")
-    public String index() {
-        return "index";
-    }
+    // @RequestMapping("/")
+    // public String index() {
+    //     return "index";
+    // }
 }

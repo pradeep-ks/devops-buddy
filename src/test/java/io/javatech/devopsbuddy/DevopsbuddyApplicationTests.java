cf0c34c4aefa0d7cec457f2001e94874d66a5e46
Pradeep Kumar <pradeepkumar0509@gmail.com>
2021-06-13 19:19:03 +0530

    Initial commit for the spring boot project

---
+++ src/test/java/io/javatech/devopsbuddy/DevopsbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javatech.devopsbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

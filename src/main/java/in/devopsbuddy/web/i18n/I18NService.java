b3bd16991186ed6de636b070d022540bbae115e3
Pradeep Kumar <pradeepkumar0509@gmail.com>
2021-06-14 12:05:44 +0530

    Added logging support with logback

--- src/main/java/in/devopsbuddy/web/i18n/I18NService.java
+++ src/main/java/in/devopsbuddy/web/i18n/I18NService.java
@@ -2,6 +2,8 @@ package in.devopsbuddy.web.i18n;
 
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -10,10 +12,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class I18NService {
     
+    private static final Logger LOGGER = LoggerFactory.getLogger(I18NService.class);
+
     @Autowired
     private MessageSource messageSource;
 
     public String getMessage(String messageId) {
+        LOGGER.info("Returning i18n text for messageId {}", messageId);
         Locale locale = LocaleContextHolder.getLocale();
         return getMessage(messageId, locale);
     }
